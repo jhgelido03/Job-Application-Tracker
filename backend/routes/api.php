07441d5b256d1b3f97a00de1718b3f9f682127f2
John Henderson Gelido <jhgelido03@gmail.com>
2025-10-15 13:16:53 +0800

    Jobs Authentication and Authorization

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
-Route::apiResource('jobs',MyJobsController::class);
+Route::apiResource('jobs',MyJobsController::class)->middleware('auth:sanctum');
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
 Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
